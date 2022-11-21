9e70e85f6dfdf4b1a631f0c3e2e6de0aaaa58440
Fenix Quiroz <fquirozdybala@gmail.com>
2022-11-20 21:16:42 -0500

    Version 1

---
+++ js/main.js
@@ -0,0 +1,135 @@
+const modalSad = document.querySelector(".modal__sad")
+const sadFeeling = document.querySelector(".sad-feeling")
+modalSad.addEventListener('click' , ()=> {
+   sadFeeling.style = "display: block;"
+});
+
+const modalHappy = document.querySelector(".modal__happy")
+const happyFeeling = document.querySelector(".happy-feeling")
+modalHappy.addEventListener('click', ()=> {
+    happyFeeling.style= "display: block"
+})
+
+
+const modalAnnoyed = document.querySelector(".modal__annoyed")
+const annoyedFeeling = document.querySelector(".annoyed-feeling")
+modalAnnoyed.addEventListener('click', ()=> {
+    annoyedFeeling.style = "display: block;"
+})
+
+
+const modalTired = document.querySelector(".modal__tired")
+const tiredFeeling = document.querySelector(".tired-feeling")
+modalTired.addEventListener('click', ()=> {
+    tiredFeeling.style = "display:block;"
+})
+
+
+const sadBtn1 = document.getElementById("sad-btn1")
+const sadTxt1 = document.querySelector(".sad-feeling__text1")
+sadBtn1.addEventListener('click', ()=> {
+    sadTxt1.style = "display: flex;"
+})
+
+const sadBtn2 = document.getElementById("sad-btn2")
+const sadTxt2 = document.querySelector(".sad-feeling__text2") 
+sadBtn2.addEventListener('click' , ()=> {
+    sadTxt2.style= "display: flex;"
+})
+
+
+const sadBtn3 = document.getElementById("sad-btn3")
+const sadTxt3 = document.querySelector(".sad-feeling__text3")
+sadBtn3.addEventListener('click' , ()=> {
+    sadTxt3.style= "display: flex;"
+})
+
+const sadBtn4 = document.getElementById("sad-btn4")
+const sadTxt4 = document.querySelector(".sad-feeling__text4")
+sadBtn4.addEventListener('click' , ()=> {
+    sadTxt4.style= "display: flex;"
+})
+
+
+const sadBtn5 = document.getElementById("sad-btn5")
+const sadTxt5 = document.querySelector(".sad-feeling__text5")
+sadBtn5.addEventListener('click' , ()=> {
+    sadTxt5.style= "display: flex;"
+})
+
+//happy 
+const happyBtn1 = document.getElementById("happy-btn1")
+const happyTxt1 = document.querySelector(".happy-feeling__text1")
+happyBtn1.addEventListener('click' , ()=> {
+    happyTxt1.style= "display: flex;"
+})
+
+const happyBtn2 = document.getElementById("happy-btn2")
+const happyTxt2 = document.querySelector(".happy-feeling__text2")
+happyBtn2.addEventListener('click' , ()=> {
+    happyTxt2.style= "display: flex;"
+})
+
+const happyBtn3 = document.getElementById("happy-btn3")
+const happyTxt3 = document.querySelector(".happy-feeling__text3")
+happyBtn3.addEventListener('click' , ()=> {
+    happyTxt3.style= "display: flex;"
+})
+
+const happyBtn4 = document.getElementById("happy-btn4")
+const happyTxt4 = document.querySelector(".happy-feeling__text4")
+happyBtn4.addEventListener('click' , ()=> {
+    happyTxt4.style= "display: flex;"
+})
+
+//annoyed
+
+const annoyedBtn1 = document.getElementById("annoyed-btn1")
+const annoyedTxt1 = document.querySelector(".annoyed-feeling__text1")
+annoyedBtn1.addEventListener('click' , ()=> {
+    annoyedTxt1.style= "display: flex;"
+})
+
+const annoyedBtn2 = document.getElementById("annoyed-btn2")
+const annoyedTxt2 = document.querySelector(".annoyed-feeling__text2")
+annoyedBtn2.addEventListener('click' , ()=> {
+    annoyedTxt2.style= "display: flex;"
+    
+})
+
+const annoyedBtn3 = document.getElementById("annoyed-btn3")
+const annoyedTxt3 = document.querySelector(".annoyed-feeling__text3")
+annoyedBtn3.addEventListener('click' , ()=> {
+    annoyedTxt3.style= "display: flex;"
+})
+
+const annoyedBtn4 = document.getElementById("annoyed-btn4")
+const annoyedTxt4 = document.querySelector(".annoyed-feeling__text4")
+annoyedBtn4.addEventListener('click' , ()=> {
+    annoyedTxt4.style= "display: flex;"
+})
+
+//tired
+const tiredBtn1 = document.getElementById("tired-btn1")
+const tiredTxt1 = document.querySelector(".tired-feeling__text1")
+tiredBtn1.addEventListener('click' , ()=> {
+    tiredTxt1.style= "display: flex;"
+})
+
+const tiredBtn2 = document.getElementById("tired-btn2")
+const tiredTxt2 = document.querySelector(".tired-feeling__text2")
+tiredBtn2.addEventListener('click' , ()=> {
+    tiredTxt2.style= "display: flex;"
+})
+
+const tiredBtn3 = document.getElementById("tired-btn3")
+const tiredTxt3 = document.querySelector(".tired-feeling__text3")
+tiredBtn3.addEventListener('click' , ()=> {
+    tiredTxt3.style= "display: flex;"
+})
+
+const tiredBtn4 = document.getElementById("tired-btn4")
+const tiredTxt4 = document.querySelector(".tired-feeling__text4")
+tiredBtn4.addEventListener('click' , ()=> {
+    tiredTxt4.style= "display: flex;"
+})
